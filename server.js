1fc7ed4a1852f3424c0d1bcc3ef318bb2750abf8
Lydia Kim <lydiakim47@gmail.com>
2022-09-20 19:01:30 -0700

    port fixed

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static('public'));
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT} 🚀`);
+  console.log(`App listening at http://localhost:${PORT}`);
 });
